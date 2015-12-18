92aa9beb4be23466542a7fb9c60a7c0adee48716
DafneBar <dafne.abad@gmail.com>
2015-12-18 16:49:41 -0600

    con las consultas a la base

--- controllers/documentos/captura_controller.php
+++ controllers/documentos/captura_controller.php
@@ -5,6 +5,16 @@ $header['js'][] = 'captura_documentos';
 
 $contenido = 'documentos/captura';
 
+$tipo = seleccionarTodo("*","cTipoDocumental","1 order by asc");
+while($row=mysqli_fetch_array($tipo[1])){
+	echo "<br>value ".$row[0];
+	echo "<br>Text ".$row[1];}
+
+$areaAtencion = seleccionarTodo("area,idArea","cArea","areaPadre=3");
+while($row=mysqli_fetch_array($areaAtencion[1])){
+	echo "<br>value ".$row[0];
+	echo "<br>Text ".$row[1];}
+	
 $data = array(
 		'header' => $header,
 		'contenido' => $contenido,
