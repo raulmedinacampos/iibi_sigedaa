33dbd79834fecd56b0fc1525e15fcbcc0652e9ab
DafneBar <dafne.abad@gmail.com>
2015-12-31 14:51:01 -0600

    cambio nombre form pero sigue sin funcionar :(

--- js/captura_documentos.js
+++ js/captura_documentos.js
@@ -2,6 +2,7 @@ function enviarDatos() {
 	$("#btn_obtener").click(function(e) {
 		e.preventDefault();
 		
+		$.post('guardaDoc_controller.php', $(formSolicitud).serialize(), function(data) {});
 		$(".modal-dialog").addClass("modal-sm");
 		$(".modal-title").html('Recepción de documentos - Información para sello');
 		$(".modal-body").html('<p>Número de folio: E / ÁREA / 1234 / 2015</p><p>Recibió: ABC</p><p>Fecha de recepción: dd/mm/aaaa</p><p>Hora: hh:mm</p>');
