895825db5bd2e8d25193f48880b182ff11e99115
DafneBar <dafne.abad@gmail.com>
2015-12-31 00:21:34 -0600

    más variables de sesion

--- controllers/verificaUsuario_controller.php
+++ controllers/verificaUsuario_controller.php
@@ -4,15 +4,23 @@ session_start();
 $usuario = (isset($_POST['usuario'])) ? addslashes($_POST['usuario']) : "";
 $contrasenia = (isset($_POST['password'])) ? addslashes($_POST['password']) : "";
 
-$usuario=seleccionarSinMsj("*","usuarioSUS","usuario='".$usuario."' and contrasenia='".$contrasenia."'");
+$usuario=seleccionarSinMsj("*","usuario","usuario='".$usuario."' and contrasenia='".$contrasenia."'");
 
 if ( is_array($usuario) && $usuario[0] != null ) {
 
 	$_SESSION['tipoUsuario'] = $usuario['tipoUsuario'];
 	$_SESSION['idUsuario']= $usuario['idUsuario'];
 	$_SESSION['idEmpleado']=$usuario['idEmpleado'];
-	$_SESSION['idUAutoriza']=$usuario['idUsuAutoriza'];
+	
+	$idArea['idArea'] = seleccionarSinMsj("idArea", "puesto","estatus=1 and idEmpleado=".$usuario['idEmpleado']);
+	
+	$siglas= seleccionarSinMsj("siglas,areaPadre", "cArea","idArea=".$idArea['idArea']);
+	$_SESSION['areaSiglas'] = $siglas['siglas'];
+	$_SESSION['idAreaPadre'] =$siglas['areaPadre'];
 		
+	$_SESSION['areaPadre'] = seleccionarSinMsj("siglas", "cArea", "idArea=".$siglas['areaPadre']);
+	
+			
 	if ( $_SESSION['tipoUsuario'] == 1 ) {
 		Flight::redirect('bandejas/expedientes');
 	} else {
