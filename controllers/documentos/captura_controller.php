277bd2b79d34d2b2a69809e8d07176b168426c44
DafneBar <dafne.abad@gmail.com>
2015-12-30 18:45:51 -0600

    tercera vez... commit y push

--- controllers/documentos/captura_controller.php
+++ controllers/documentos/captura_controller.php
@@ -10,6 +10,11 @@ while($row=mysqli_fetch_array($tipo[1])){
 	echo "<br>value ".$row[0];
 	echo "<br>Text ".$row[1];}
 
+$dependencia = seleccionarTodo("*", "cDependencia", 1);
+while($row=mysqli_fetch_array($dependencia[1])){
+	echo "<br>value ".$row[0];
+	echo "<br>Text ".$row[1];}
+
 $areaAtencion = seleccionarTodo("area,idArea","cArea","areaPadre=3");
 while($row=mysqli_fetch_array($areaAtencion[1])){
 	echo "<br>value ".$row[0];
