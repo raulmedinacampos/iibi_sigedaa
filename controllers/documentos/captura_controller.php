fc6e53549ae70fc65bf12facfb55982ca70ddd63
DafneBar <dafne.abad@gmail.com>
2015-12-31 08:59:38 -0600

    iteraciones comentadas

--- controllers/documentos/captura_controller.php
+++ controllers/documentos/captura_controller.php
@@ -6,25 +6,29 @@ $header['js'][] = 'captura_documentos';
 $contenido = 'documentos/captura';
 
 $tipo = seleccionarTodo("*","cTipoDocumental","1 order by asc");
-while($row=mysqli_fetch_array($tipo[1])){
+/*while($row=mysqli_fetch_array($tipo[1])){
 	echo "<br>value ".$row[0];
 	echo "<br>Text ".$row[1];}
+*/
 
 $dependencia = seleccionarTodo("*", "cDependencia", 1);
-while($row=mysqli_fetch_array($dependencia[1])){
+/*while($row=mysqli_fetch_array($dependencia[1])){
 	echo "<br>value ".$row[0];
 	echo "<br>Text ".$row[1];}
+*/
 
 $areaAtencion = seleccionarTodo("area,idArea","cArea","areaPadre=3");
-while($row=mysqli_fetch_array($areaAtencion[1])){
+/*while($row=mysqli_fetch_array($areaAtencion[1])){
 	echo "<br>value ".$row[0];
 	echo "<br>Text ".$row[1];}
-	
+*/
+
 $data = array(
 		'header' => $header,
 		'contenido' => $contenido,
 		'tipo' => $tipo,
 		'areaAtención' => $areaAtencion,
+		'dependencia' => $dependencia
 );
  
 Flight::render('template/layout', $data);
