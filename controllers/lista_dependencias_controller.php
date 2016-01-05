c835f7762db277b1d271e8425d4325cca2e8951a
Raúl Medina <raulmedinacampos@yahoo.com.mx>
2016-01-05 00:02:35 -0600

    Codificación dependencias

--- controllers/lista_dependencias_controller.php
+++ controllers/lista_dependencias_controller.php
@@ -8,7 +8,7 @@ $dependencia = seleccionarTodo("*", "cDependencia", "dependencia LIKE '%".$where
 
 if ( isset($dependencia[1]) && mysqli_num_rows($dependencia[1]) > 0 ) {
 	while( $row = mysqli_fetch_array($dependencia[1]) ) {
-		$dep[] = utf8_encode($row[1]);
+		$dep[] = $row[1];
 	}
 }
 
